09c58f6b86a8dd3577a00982cdc3172a58f595c4
Zeynep Nursena Kütük <96338249+zeynepnursenaktk@users.noreply.github.com>
2023-12-31 20:06:46 +0300

    Update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,15 @@
+const CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 9,
+    BOX_SIZE: 3,
+    NUMBERS: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    LEVEL_NAME: [
+        'Easy',
+        'Medium',
+        'Hard'
+    ],
+    LEVEL: [29, 38, 47]
+}
 
 const newGrid = (size) => {
     let arr = new Array(size);
